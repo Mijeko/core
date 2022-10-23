9a7a0eb2d2d7dc411bcd6854ca75b0e4475347d7
Konstantin <popugau@gmail.com>
2022-10-23 11:47:59 +0700

    change order steps

--- sh/22ubuntu.sh
+++ sh/22ubuntu.sh
@@ -28,12 +28,16 @@ echo \
 sudo apt-get update -y
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
 
-#setup minikube
-curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube_latest_amd64.deb
-sudo dpkg -i minikube_latest_amd64.deb
-minikube start --force
-
 #setup kubectl
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 kubectl version --client --output=yaml
+
+#setup minikube
+curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube_latest_amd64.deb
+sudo dpkg -i minikube_latest_amd64.deb
+
+# выполнить если будет ошибка docker.sock not connection
+#sudo setfacl --modify user:<user name or ID>:rw /var/run/docker.sock
+
+minikube start --force
